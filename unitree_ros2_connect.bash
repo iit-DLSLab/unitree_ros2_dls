1b1a73e62c7bb6281fcd6c1d3b211ac7813456ba
giulioturrisi <giulioturrisi@hotmail.it>
2026-07-13 16:32:33 +0200

    added clean localhost variable

--- unitree_ros2_connect.bash
+++ unitree_ros2_connect.bash
@@ -4,7 +4,8 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #CURRENT_DIR="$(pwd)"
 echo "Current dir: $CURRENT_DIR"
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_LOCALHOST_ONLY=0
 export ROS_DOMAIN_ID=0 # Set a unique domain ID for your local environment
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enx6c1ff71cdfe2" priority="default" multicast="default" />
+                            <NetworkInterface name="enp98s0" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'

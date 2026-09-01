b4693d74640f3167280a0507e2cabaeb47a759f3
giulioturrisi <giulioturrisi@hotmail.it>
2026-09-01 09:20:49 +0200

    changed topic name

--- ros2_ws/src/aliengo_hal/aliengo_hal.cpp
+++ ros2_ws/src/aliengo_hal/aliengo_hal.cpp
@@ -78,8 +78,8 @@ void LowLevelCmdNode::Init() {
 
   // Create publishers and subscribers to talk with the controller/DLS2
   imu_pub_ = this->create_publisher<dls2_interface::msg::Imu>("/imu", 1);
-  blind_state_pub_ = this->create_publisher<dls2_interface::msg::BlindState>("/blind_state_quadruped", 1);
-  joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states_quadruped", 1);
+  blind_state_pub_ = this->create_publisher<dls2_interface::msg::BlindState>("/blind_state_legged", 1);
+  joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states_legged", 1);
   joint_state_.name = {
       "FL_hip_joint", "FL_thigh_joint", "FL_calf_joint",
       "FR_hip_joint", "FR_thigh_joint", "FR_calf_joint",
@@ -87,7 +87,7 @@ void LowLevelCmdNode::Init() {
       "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"};
   blind_state_.joints_name = joint_state_.name;
   control_signal_sub_ = this->create_subscription<dls2_interface::msg::ControlSignal>(
-      "/control_signal_quadruped", 1, [this](const dls2_interface::msg::ControlSignal::SharedPtr msg) {
+      "/control_signal_legged", 1, [this](const dls2_interface::msg::ControlSignal::SharedPtr msg) {
         ControlSignalMessageHandler(msg);
       });
 }
